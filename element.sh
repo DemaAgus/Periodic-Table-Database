84e5a6b05c5bf922b51b97a71ff5271b3bb5c11b
Agustin De Marchi <agusdemarchi03@gmail.com>
2024-12-25 22:54:40 +0000

    feat: add validation for missing argument

--- element.sh
+++ element.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Check if no arguments are provided
 if [ -z "$1" ]; then
-  echo "Please provide an element as an argument."
-  exit 1
+    echo "Please provide an element as an argument."
+    exit 0
 fi
