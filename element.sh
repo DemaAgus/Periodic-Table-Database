d151c2f2d999d00bedb8b7e47cc396891051d635
Agustin De Marchi <agusdemarchi03@gmail.com>
2024-12-25 23:01:14 +0000

    feat: response if the element was found in the database

--- element.sh
+++ element.sh
@@ -14,4 +14,8 @@ WHERE atomic_number::TEXT = '$1' OR symbol = '$1' OR name = '$1';")
 
 if [[ -z "$ELEMENT" ]]; then
     echo "I could not find that element in the database."
+else
+    echo "$ELEMENT" | while read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING; do
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+    done
 fi
