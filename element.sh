0a2b58026f16067915bd5753744c7a6353bcb4b5
Agustin De Marchi <agusdemarchi03@gmail.com>
2024-12-25 22:56:14 +0000

    feat: search the element

--- element.sh
+++ element.sh
@@ -4,3 +4,10 @@ if [ -z "$1" ]; then
     echo "Please provide an element as an argument."
     exit 0
 fi
+
+ELEMENT=$(psql -U username -d database_name -t -c "
+SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+FROM elements 
+JOIN properties USING(atomic_number) 
+JOIN types USING(type_id) 
+WHERE atomic_number::TEXT = '$1' OR symbol = '$1' OR name = '$1';")
