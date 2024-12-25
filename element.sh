6c06ca18c5777109b6c24f4def8530bdf7981afb
Agustin De Marchi <agusdemarchi03@gmail.com>
2024-12-25 22:59:08 +0000

    feat: response if the element could not be found int the database

--- element.sh
+++ element.sh
@@ -11,3 +11,7 @@ FROM elements
 JOIN properties USING(atomic_number) 
 JOIN types USING(type_id) 
 WHERE atomic_number::TEXT = '$1' OR symbol = '$1' OR name = '$1';")
+
+if [[ -z "$ELEMENT" ]]; then
+    echo "I could not find that element in the database."
+fi
